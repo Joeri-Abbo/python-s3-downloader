2be92436856b014beb6e1b63748e4a5b00aed5d7
Joeri Abbo <joeriabbo@hotmail.com>
2023-03-21 19:02:15 +0100

    Improve script

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 import boto3
+import os
 
 if __name__ == '__main__':
     s3 = boto3.client('s3')
     response = s3.list_buckets()
 
-    with open("downloader.sh", "w") as file:
+    path = os.path.abspath(os.getcwd())
 
+    with open("downloader.sh", "w") as file:
         for bucket in response['Buckets']:
+            file.write('')
+            file.write(f'echo "{bucket["Name"]}"\n')
+            file.write(f"if [ -e {bucket['Name']}.tar.gz ]\n")
+            file.write(f"then\n")
+            file.write(f"tar -xvf {bucket['Name']}.tar.gz\n")
+            file.write(f"fi\n")
             file.write(f'aws s3 sync s3://{bucket["Name"]} {bucket["Name"]}\n')
+            file.write(f"tar -czvf {bucket['Name']}.tar.gz {bucket['Name']}\n")
+            file.write(f"rm -rf {bucket['Name']}\n")
             file.write('')
